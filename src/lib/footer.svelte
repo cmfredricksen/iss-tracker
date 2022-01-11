c97c2bbfc7c2374391f9fcd1b1983829bd7b23b0
Christine Fredricksen <cmfredricksen@gmail.com>
2022-01-10 21:32:06 -0600

    added header, footer, and nav

---
+++ src/lib/footer.svelte
@@ -0,0 +1,10 @@
+<footer>
+	<h4>© 2022 Chris Fredricksen • All Rights Reserved ♥</h4>
+</footer>
+
+<style>
+	footer {
+		text-align: center;
+		padding: 1rem 0;
+	}
+</style>
---
+++ src/lib/header.svelte
@@ -0,0 +1,6 @@
+<script>
+	import Nav from './nav.svelte';
+</script>
+
+<h1>Interesting Things</h1>
+<Nav />
